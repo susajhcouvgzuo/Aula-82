64f783723e3ddeb993bda5978227bf7b8c731df5
susajhcouvgzuo <131784759+susajhcouvgzuo@users.noreply.github.com>
2024-01-12 10:59:08 -0300

    Add files via upload

---
+++ C82.js
@@ -0,0 +1,53 @@
+console.log("Meu primeiro projeto no console");
+
+            var firstName = "Jo";
+            var lastName = "Onn";
+
+/*
+Concatene o primeiro e último nome para imprimir o nome inteiro da pessoa no console. 
+
+Digite a linha: "firstNameconcat(lastName)"
+*/
+            var fullName = firstName.concat(lastName) ;
+            console.log(fullName);
+
+            var a = 10;
+            var b = 5;
+/*
+Crie uma variável chamada add e guarda o valor da adição de a e b neste variável.
+
+Digite: var add = a + b
+
+Então, imprima a variável para exibir o valor da adição
+
+Digite: console.log(add)
+*/
+            var add = a + b ;
+            console.log(add);
+
+/*
+Crie uma variável chamada sub e guarde o valor da subtração de a e b nesta variável.
+
+Digite: var sub = a - b
+
+Então, imprima a variável para exibir o valor da subtração
+
+Digite: console.log(sub)
+*/
+            var sub = a - b ;
+            console.log(sub);
+
+/*
+Crie uma variável chamada multiple e guarde o valor da multiplicação de a e b nesta variável.
+
+Digite: var multiple = a * b
+
+Então, imprima a variável para exibir o valor da multiplicação
+
+Digite: console.log(multiple)
+*/
+            var multiple = a * b ;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
